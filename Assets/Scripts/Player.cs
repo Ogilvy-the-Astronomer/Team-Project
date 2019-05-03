49a28d5d33e8cd4654952c3f7841ced54e368a3f
Howard van Waard <thethirdnexusofalltime@gmail.com>
2019-05-03 14:05:42 +0100

    made movement smoother

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,9 @@ public class Player : MonoBehaviour {
 	public float sensitivity;
 	public float jumpForce;
 	public GameObject root;
+	public bool isMoving;
+	public Vector3 MoveDir;
+	float moveCounter;
 
 	// equipment variables
 	public Weapon WeaponPrimary;
@@ -22,30 +25,57 @@ public class Player : MonoBehaviour {
 	void Start () {
 		moveAction = true;
 		standardAction = true;
+		isMoving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Camera.main.transform.position = transform.position + new Vector3 (0, 75, 0);
 		if (moveAction) {
-			if (Input.GetKeyDown(KeyCode.W) && !Physics.Raycast(new Ray(transform.position + Vector3.up,transform.forward),1f)) {
-				transform.Translate (transform.forward);
-				//moveAction = false;
+			if (Input.GetKey(KeyCode.W) && !Physics.Raycast(new Ray(transform.position + Vector3.up,Vector3.forward),1f)) {
+				//transform.Translate (Vector3.forward);
+				moveAction = false;
+				MoveDir = Vector3.forward;
+				isMoving = true;
+				root.transform.eulerAngles = new Vector3 (0, 0, 0);
 				//Debug.Log ("Player moved");
 			}
-			if (Input.GetKeyDown(KeyCode.S) && !Physics.Raycast(new Ray(transform.position + Vector3.up,-transform.forward),1f)) {
-				transform.Translate (-transform.forward);
-				//moveAction = false;
+			if (Input.GetKey(KeyCode.S) && !Physics.Raycast(new Ray(transform.position + Vector3.up,-Vector3.forward),1f)) {
+				//transform.Translate (-Vector3.forward);
+				moveAction = false;
+				MoveDir = -Vector3.forward;
+				isMoving = true;
+				root.transform.eulerAngles = new Vector3 (0, 180, 0);
 				//Debug.Log ("Player moved");
 			}
-			if (Input.GetKeyDown(KeyCode.D) && !Physics.Raycast(new Ray(transform.position + Vector3.up,transform.right),1f)) {
-				transform.Translate (transform.right);
-				//moveAction = false;
+			if (Input.GetKey(KeyCode.D) && !Physics.Raycast(new Ray(transform.position + Vector3.up,Vector3.right),1f)) {
+				//transform.Translate (Vector3.right);
+				moveAction = false;
+				MoveDir = Vector3.right;
+				isMoving = true;
+				root.transform.eulerAngles = new Vector3 (0, 90, 0);
 				//Debug.Log ("Player moved");
 			}
-			if (Input.GetKeyDown(KeyCode.A) && !Physics.Raycast(new Ray(transform.position + Vector3.up,-transform.right),1f)) {
-				transform.Translate (-transform.right);
-				//moveAction = false;
+			if (Input.GetKey(KeyCode.A) && !Physics.Raycast(new Ray(transform.position + Vector3.up,-Vector3.right),1f)) {
+				//transform.Translate (-Vector3.right);
+				moveAction = false;
+				MoveDir = -Vector3.right;
+				isMoving = true;
+				root.transform.eulerAngles = new Vector3 (0,270, 0);
 				//Debug.Log ("Player moved");
+			}
+		}
+		if (isMoving) {
+			if (moveCounter < 1/moveSpeed) {
+				GetComponent<Animator> ().SetBool ("isRunning", true);
+				transform.Translate (MoveDir / (1/moveSpeed));
+				moveCounter++;
+
+			} else {
+				isMoving = false;
+				GetComponent<Animator> ().SetBool ("isRunning", false);
+				moveAction = true;
+				moveCounter = 0;
 			}
 		}
 	}
@@ -60,7 +90,7 @@ public class Player : MonoBehaviour {
 			Debug.Log ("Attack up");
 			standardAction = false;
 			RaycastHit hit;
-			Physics.Raycast (new Ray (transform.position, transform.forward), out hit, 1f);
+			Physics.Raycast (new Ray (transform.position, Vector3.forward), out hit, 1f);
 			hit.transform.gameObject.GetComponent<EnemyController> ().TakeDamage (5);
 		}
 	}
@@ -69,7 +99,7 @@ public class Player : MonoBehaviour {
 			Debug.Log ("Attack left");
 			standardAction = false;
 			RaycastHit hit;
-			Physics.Raycast (new Ray (transform.position, -transform.right), out hit, 1f);
+			Physics.Raycast (new Ray (transform.position, -Vector3.right), out hit, 1f);
 			hit.transform.gameObject.GetComponent<EnemyController> ().TakeDamage (5);
 		}
 	}
@@ -78,7 +108,7 @@ public class Player : MonoBehaviour {
 			Debug.Log ("Attack down");
 			standardAction = false;
 			RaycastHit hit;
-			Physics.Raycast (new Ray (transform.position, -transform.forward), out hit, 1f);
+			Physics.Raycast (new Ray (transform.position, -Vector3.forward), out hit, 1f);
 			hit.transform.gameObject.GetComponent<EnemyController> ().TakeDamage (5);
 		}
 	}
@@ -87,7 +117,7 @@ public class Player : MonoBehaviour {
 			Debug.Log ("Attack right");
 			standardAction = false;
 			RaycastHit hit;
-			Physics.Raycast (new Ray (transform.position, transform.right), out hit, 1f);
+			Physics.Raycast (new Ray (transform.position, Vector3.right), out hit, 1f);
 			hit.transform.gameObject.GetComponent<EnemyController> ().TakeDamage (5);
 		}
 	}
