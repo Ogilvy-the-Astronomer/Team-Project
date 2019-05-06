4e7b0a7406fb1ef8ff845febcc7ff414ed066706
CucumberChariot <kiszonkorniszon@gmail.com>
2019-05-07 00:07:04 +0100

    i wish for the sweet release
    please

---
+++ Assets/Scripts/InvButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvButtonScript : MonoBehaviour
+{
+    public Item Item;
+    public Button ButtonParent;
+    public string Text;
+
+    void Start() { }
+    void Update() { }
+
+    public void SetFields(string text, Item item)
+    {
+        Text = text;
+        Item = item;
+        ButtonParent.GetComponentInChildren<Text>().text = Text;
+    }
+}
